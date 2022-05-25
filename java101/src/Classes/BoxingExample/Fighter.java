0d39bcc2a16da03452723ac3048ff42010dcfa6f
Orhun YILDIZ <orhunyyildiz@gmail.com>
2022-05-25 18:08:20 +0300

    BoxingExample

---
+++ java101/src/Classes/BoxingExample/Fighter.java
@@ -0,0 +1,34 @@
+package Classes.BoxingExample;
+
+public class Fighter {
+    String name;
+    int damage;
+    int health;
+    int weight;
+    double dodge;
+
+    public Fighter(String name, int damage, int health, int weight, double dodge) {
+        this.name = name;
+        this.damage = damage;
+        this.health = health;
+        this.weight = weight;
+        this.dodge = dodge;
+    }
+
+    public int hit(Fighter foe) {
+        System.out.println("------------");
+        System.out.println(this.name + " => " + foe.name + " " +  this.damage + " hasar vurdu.");
+        if (foe.isDodge()) {
+            System.out.println(foe.name + " gelen hasarı savurdu.");
+            return foe.health;
+        }
+        if (foe.health - damage < 0)
+            return 0;
+        return foe.health - damage;
+    }
+
+    public boolean isDodge() {
+        double randomValue = Math.random() * 100;
+        return randomValue <= dodge;
+    }
+}
